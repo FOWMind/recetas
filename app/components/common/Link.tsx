e8c30a94155c3b4e001719c33d99dbb53781cf2a
FOWMind <alwl1@hotmail.com>
2024-05-04 06:58:11 -0300

    style: cursor default if Link does not use valid href

--- app/components/common/Link.tsx
+++ app/components/common/Link.tsx
@@ -3,9 +3,10 @@ import NextLink from "next/link"
 
 export const Link = ({ className = "", href, as, ...props }: LinkProps) => {
   const As = as ?? NextLink
+  const isLink = href !== "" && href !== "#"
   return (
     <As
-      className={`inline-block p-2 font-medium text-black hover:text-neutral-800 ${className}`}
+      className={`inline-block ${!isLink ? "cursor-default" : ""} p-2 font-medium text-black hover:text-neutral-800 ${className}`}
       href={!as ? href : null}
       {...props}
     />
