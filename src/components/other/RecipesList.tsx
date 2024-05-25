efc41166d2e820c047e8c679e0746ff7473d0948
FOWMind <alwl1@hotmail.com>
2024-05-25 04:57:56 -0300

    style(RecipesList): auto-fill grid

--- src/components/other/RecipesList.tsx
+++ src/components/other/RecipesList.tsx
@@ -1,11 +1,13 @@
-import type { RecipesListProps } from "@/types"
-import { RecipeCard } from "@/components"
+import type { RecipesListProps } from '@/types'
+import { RecipeCard } from '@/components'
 
-export const RecipesList = ({ className = "", recipes, ...props }: RecipesListProps) => {
-  return (
-    <section className={`grid grid-cols-3 gap-6 ${className}`} {...props}>
-      {recipes.length > 0 &&
-        recipes.map(({ id, thumbnail, ...props }) => <RecipeCard key={id} image={thumbnail} {...props} />)}
-    </section>
-  )
+export const RecipesList = ({ className = '', recipes, ...props }: RecipesListProps) => {
+	return (
+		<section
+			className={`grid [grid-template-columns:repeat(auto-fill,minmax(300px,1fr))] gap-6 ${className}`}
+			{...props}>
+			{recipes.length > 0 &&
+				recipes.map(({ id, thumbnail, ...props }) => <RecipeCard key={id} image={thumbnail} {...props} />)}
+		</section>
+	)
 }
