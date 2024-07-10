48ab7d34bcde97a9a80b7832ecfee64daa16e969
FOWMind <alwl1@hotmail.com>
2024-07-10 00:21:46 -0300

    unknown

--- src/app/[locale]/signup/page.tsx
+++ src/app/[locale]/signup/page.tsx
@@ -1,18 +1,17 @@
 import { Button, Field, Headline, Link } from '@/components'
 import { useTranslations } from 'next-intl'
 
-export default function LoginPage() {
+export default function SignUpPage() {
 	const t = useTranslations('AuthPage')
 	return (
 		<section>
 			<form className="[&>*]:max-w-none max-w-sm mx-auto flex flex-col gap-y-2">
-				<Headline size="sm">{t('loginTitle')}</Headline>
+				<Headline size="sm">{t('registerTitle')}</Headline>
 				<Field placeholder={t('emailOrUsername')} />
 				<Field type="password" placeholder={t('password')} />
 				<Button variant="featured">{t('submit')}</Button>
 				<div className="flex flex-col">
-					<Link href="/forgot-password">{t('forgotPassword')}</Link>
-					<Link href="/signup">{t('createAccount')}</Link>
+					<Link href="/login">{t('login')}</Link>
 				</div>
 			</form>
 		</section>
--- src/components/common/Link.tsx
+++ src/components/common/Link.tsx
@@ -1,14 +1,15 @@
-import type { LinkProps } from "@/types"
-import NextLink from "next/link"
+import type { LinkProps } from '@/types'
+import NextLink from 'next/link'
 
-export const Link = ({ className = "", href, as, ...props }: LinkProps) => {
-  const As = as ?? NextLink
-  const isLink = href !== "" && href !== "#"
-  return (
-    <As
-      className={`inline-block ${!isLink ? "cursor-default" : ""} p-2 font-medium text-black hover:text-neutral-800 dark:text-white dark:hover:text-neutral-300 ${className}`}
-      href={!as ? href : null}
-      {...props}
-    />
-  )
+export const Link = ({ className = '', href, as, ...props }: LinkProps) => {
+	const As = as ?? NextLink
+	const isLink = href !== '' && href !== '#'
+	return (
+		<As
+			className={`inline-block ${!isLink ? 'cursor-default' : ''} p-2 font-medium text-black hover:text-neutral-800 dark:text-white dark:hover:text-neutral-300 ${className}`}
+			href={!as ? href : null}
+			locale={As === NextLink ? false : null}
+			{...props}
+		/>
+	)
 }
