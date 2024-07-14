57c882b7ebb1782fe055cdc8fae7b05ff629c13a
Mind <alwl1@hotmail.com>
2024-07-14 06:27:18 -0300

    style(pagination button): set visible color when it is featured and disabled

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,13 +2,13 @@ import type { ButtonProps } from '@/types'
 
 enum variants {
 	default = 'bg-white text-black dark:bg-black dark:text-white',
-	featured = 'bg-black text-white dark:bg-white dark:text-black',
+	featured = 'bg-black text-white dark:bg-white dark:text-black disabled:text-black dark:disabled:text-white',
 }
 
 export const Button = ({ className = '', as: As = 'button', variant = 'default', ...props }: ButtonProps) => {
 	return (
 		<As
-			className={`inline-block rounded-full px-10 py-3 text-center font-medium ${variant && variants[variant]} disabled:opacity-50 disabled:bg-transparent dark:disabled:bg-transparent ${className}`}
+			className={`inline-block rounded-full px-10 py-3 text-center font-medium disabled:bg-transparent dark:disabled:bg-transparent disabled:opacity-20 ${variant && variants[variant]} ${className}`}
 			{...props}
 		/>
 	)
