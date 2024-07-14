7608947e173c148a821c29a96f738fd170d287e7
Mind <alwl1@hotmail.com>
2024-07-14 06:43:02 -0300

    style(toggle theme button): taking base styles font size

--- src/components/common/Paragraph.tsx
+++ src/components/common/Paragraph.tsx
@@ -1,5 +1,5 @@
 import type { ParagraphProps } from '@/types'
 
 export const Paragraph = ({ as: As = 'p', className = '', ...props }: ParagraphProps) => {
-	return <As className={`text-base text-black dark:text-white ${className}`} {...props} />
+	return <As className={`text-black dark:text-white ${className}`} {...props} />
 }
